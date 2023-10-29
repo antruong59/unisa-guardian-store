a57f78e71df3b174a311f43cb6beb21059c88c8c
JuiceShopBot <61591748+JuiceShopBot@users.noreply.github.com>
2023-10-29 00:22:43 +0000

    Auto-fix linting issues
    Signed-off-by: JuiceShopBot <61591748+JuiceShopBot@users.noreply.github.com>

--- routes/order.ts
+++ routes/order.ts
@@ -158,7 +158,7 @@ module.exports = function placeOrder () {
             addressId: req.body.orderDetails ? req.body.orderDetails.addressId : null,
             orderId: orderId,
             delivered: false,
-            email: (email ? email : undefined),
+            email: (email || undefined),
             totalPrice: totalPrice,
             products: basketProducts,
             bonus: totalPoints,
